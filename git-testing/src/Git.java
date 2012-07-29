b31f7a1340a35db84f9081999ff300548381efee
teknokrat <iaptekar@gmail.com>
2012-07-29 15:37:21 +0100

    from intellij

---
+++ git-testing/src/Git.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: teknokrat
+ * Date: 29/07/12
+ * Time: 15:36
+ * To change this template use File | Settings | File Templates.
+ */
+public class Git {
+}
